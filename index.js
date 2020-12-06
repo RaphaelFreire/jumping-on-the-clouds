9c57d1affefb310feca3160acd48e51cef52187d
Raphael Freire <raphaelmachadofreire@gmail.com>
2020-12-06 19:07:00 -0300

    Exercicio Resolvido

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+// No exemplo abaixo há 7 núvens: 2 com raios e 5 normais.
+// 0 representa nuvens que ela PODE pular 
+// 1 representa nuvens que ela NÃO PODE pular
+
+// 4 pulos
 const cenario1 = [0, 0, 1, 0, 0, 1, 0];
+
+//3 pulos
 const cenario2 = [0, 0, 0, 0, 1, 0];
 
 const jumpingOnClouds = c => {
+  
+  let len = c.length;
+  let count = -1;
+
+  for(i = 0; i<len; i++, count++){
+
+    if (i+2 < len && c[i+2] == 0){
+      i++
+    } 
+  }
+  console.log(count)
 
 }
 
